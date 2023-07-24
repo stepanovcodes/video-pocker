5e5ba3548dc8b1a42fab2ac1a9333be38c808400
Alex Stepanov <stepanov.hw@gmail.com>
2023-07-24 13:46:32 -0600

    work on static css styling of html elements

--- js/main.js
+++ js/main.js
@@ -2,7 +2,8 @@ console.log(`JS loaded`);
 
 
 /*----- constants -----*/
-
+const suits = ['s', 'c', 'd', 'h'];
+const ranks = ['02', '03', '04', '05', '06', '07', '08', '09', '10', 'J', 'Q', 'K', 'A'];
 
 
 /*----- app's state (variables) -----*/
